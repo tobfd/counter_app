3d6bac6558820e034ca599e6f08326183e5cad9a
Tobias Schmitt <support@tobfd.de>
2025-07-20 21:22:05 +0200

    fix: add missing import

--- app/src/main/java/com/tobfd/counter/MainActivity.kt
+++ app/src/main/java/com/tobfd/counter/MainActivity.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
