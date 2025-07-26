5a2e624db3b90022c0fbf34ab7e332f701d19901
Tobias Schmitt <support@tobfd.de>
2025-07-26 19:15:05 +0200

    remove: unused import

--- app/src/main/java/com/tobfd/counter/MainActivity.kt
+++ app/src/main/java/com/tobfd/counter/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.compose.runtime.collectAsState
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.material.icons.filled.Settings
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.core.animateFloatAsState
