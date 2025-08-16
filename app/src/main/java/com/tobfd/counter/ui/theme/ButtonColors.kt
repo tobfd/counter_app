0cf73b052de22c2970cb220619ecbc1a193517f5
Tobias Schmitt <support@tobfd.de>
2025-08-17 01:30:02 +0200

    refactor: Remove unused MaterialTheme import in counterButtonColors function

--- app/src/main/java/com/tobfd/counter/ui/theme/ButtonColors.kt
+++ app/src/main/java/com/tobfd/counter/ui/theme/ButtonColors.kt
@@ -2,7 +2,6 @@ package com.tobfd.counter.ui.theme
 
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
@@ -12,7 +11,6 @@ import com.tobfd.counter.SettingsDataStore
 
 @Composable
 fun counterButtonColors(): ButtonColors {
-    val colorScheme = MaterialTheme.colorScheme
     val context = LocalContext.current
     val settingsDataStore = remember { SettingsDataStore(context) }
     val isDark = getUseDarkTheme(settingsDataStore)
